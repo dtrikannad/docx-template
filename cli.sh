eef7a8afd669d93525bab1e0d34250dfeac4159d
Deep Trikannad <deep.trikannad@acceledgy.com>
2025-11-09 00:06:43 -0800

    fixed cli.sh

--- cli.sh
+++ cli.sh
@@ -1,4 +1,3 @@
 # rm -rf New\ Document.docx; npx tsx src/docx.ts -l ./old/data/acmelaw.png -d ./sampledata.json; open New\ Document.docx;
 
-rm -rf New\ Document.docx; npx tsx src/docx.ts -l ./old/data/Picture1.jpg -d ./data1.json -o ../old/data; # open New\ Document.docx;
-#test
+rm -rf New\ Document.docx; npx tsx src/docx.ts -l ./old/data/Picture1.jpg -d ./data1.json -o ../../output; # open New\ Document.docx;
